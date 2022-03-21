c4e253bfa30dd2ab64b0c07e8d0925a34fa008ee
rsuubinn <rsuubinn@gmail.com>
2022-03-22 02:54:39 +0900

    #6.11 Our First Query

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,48 +1,20 @@
-const videos = [
-  {
-    title: 1,
-    rating: 1,
-    comments: 1,
-    createdAt: 1,
-    views: 1,
-    id: 1,
-  },
-  {
-    title: 2,
-    rating: 2,
-    comments: 2,
-    createdAt: 2,
-    views: 2,
-    id: 2,
-  },
-  {
-    title: 3,
-    rating: 3,
-    comments: 3,
-    createdAt: 3,
-    views: 3,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  return res.render("home", { pageTitle: "Home" });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: video.title, video });
+
+  return res.render("watch", { pageTitle: "Watching" });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: video.title, video });
+  return res.render("edit", { pageTitle: "Editing" });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
