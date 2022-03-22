7386e96dda6b71c16ad04fd6e9bc8ce2a376b758
rsuubinn <rsuubinn@gmail.com>
2022-03-22 23:53:35 +0900

    #6.18 More Schema

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
+  title: { type: String, required: true },
+  description: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, required: true },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, required: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
