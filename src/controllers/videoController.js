34557ce35eb711d1d518aba301a5f37a85501ea9
rsuubinn <rsuubinn@gmail.com>
2022-03-23 02:20:36 +0900

    #6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,7 +33,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -48,7 +48,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
