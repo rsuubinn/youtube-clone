eebafdffdd83f08fe2dc0f335e21084a39134ae1
rsuubinn <rsuubinn@gmail.com>
2022-03-25 03:27:33 +0900

    #7.4 Status Codes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,14 +7,14 @@ export const postJoin = async (req, res) => {
   const pageTitle = "join";
   const { name, username, email, password, password2, location } = req.body;
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
     });
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This email/username is already taken.",
     });
