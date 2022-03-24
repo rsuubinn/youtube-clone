17079cf26986f9a5faecc60554e1fdc024b48eec
rsuubinn <rsuubinn@gmail.com>
2022-03-24 23:28:29 +0900

    #7.2 Creating Account part Three

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/youtube");
+mongoose.connect("mongodb://127.0.0.1:27017/youtube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
--- src/models/User.js
+++ src/models/User.js
@@ -8,5 +8,9 @@ const userSchema = new mongoose.Schema({
   location: String,
 });
 
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
+});
+
 const User = mongoose.model("User", userSchema);
 export default User;
