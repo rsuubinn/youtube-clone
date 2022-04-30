93fbd135937ea82f93bdc0d0b85f779e80106161
rsuubinn <rsuubinn@gmail.com>
2022-04-30 16:30:32 +0900

    #8.14 Bugfix

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,6 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner");
-  console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Not found" });
   }
@@ -18,18 +17,30 @@ export const watch = async (req, res) => {
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
   const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Not found" });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.exists({ _id: id });
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById({ _id: id });
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Not found" });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
   }
   const { title, description, hashtags } = req.body;
   await Video.findByIdAndUpdate(id, {
@@ -72,7 +83,20 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById(id);
+  const user = await User.findById(_id);
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
+  }
   await Video.findByIdAndDelete(id);
+  user.videos.splice(user.videos.indexOf(id), 1);
+  user.save();
   return res.redirect("/");
 };
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,16 +11,23 @@ import { protectorMiddleware, videoUpload } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// 비디오 보기
 videoRouter.get("/:id([0-9a-f]{24})", watch);
+
+// 비디오 수정
 videoRouter
   .route("/:id([0-9a-f]{24})/edit")
   .all(protectorMiddleware)
   .get(getEdit)
   .post(postEdit);
+
+// 비디오 삭제
 videoRouter
   .route("/:id([0-9a-f]{24})/delete")
   .all(protectorMiddleware)
   .get(deleteVideo);
+
+// 비디오 업로드
 videoRouter
   .route("/upload")
   .all(protectorMiddleware)
