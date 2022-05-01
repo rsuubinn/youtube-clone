b4b77950c6dd62bcc993c616d124676cb33ef319
rsuubinn <rsuubinn@gmail.com>
2022-05-01 23:53:55 +0900

    #9.2 Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
+const path = require("path");
+
 module.exports = {
+  mode: "development",
   // 변경할 파일 경로
   entry: "./src/client/js/main.js",
   output: {
     filename: "main.js", // 변경 후 파일 이름
-    path: "./assets/js", // 저장할 경로
+    path: path.resolve(__dirname, "assets", "js"), // 저장할 경로
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
