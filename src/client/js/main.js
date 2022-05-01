aaa52d3031ee9cb1981446b3d383e7b6d3a0e0e3
rsuubinn <rsuubinn@gmail.com>
2022-05-01 22:56:43 +0900

    #9.1 Webpack Configuration part One

---
+++ src/client/js/main.js
@@ -0,0 +1,6 @@
+const hello = async () => {
+  alert("Hi, It's working!");
+  const x = await fetch("");
+};
+
+hello();
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 변경할 파일 경로
+  entry: "./src/client/js/main.js",
+  output: {
+    filename: "main.js", // 변경 후 파일 이름
+    path: "./assets/js", // 저장할 경로
+  },
+};
