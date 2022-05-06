e18570539d87161021836148a7b6d501bc4e7b5f
rsuubinn <rsuubinn@gmail.com>
2022-05-07 00:24:33 +0900

    #13.2 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,32 @@
 const recordBtn = document.getElementById("recordBtn");
 const video = document.getElementById("preview");
 
-const handleRecord = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStart = () => {
+  recordBtn.innerText = "Stop Recording";
+  recordBtn.removeEventListener("click", handleStart);
+  recordBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("Recording Done");
+    console.log(e);
+    console.log(e.data);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const handleStop = () => {
+  recordBtn.innerText = "Start Recording";
+  recordBtn.removeEventListener("click", handleStop);
+  recordBtn.addEventListener("click", handleStart);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
@@ -10,4 +34,6 @@ const handleRecord = async () => {
   video.play();
 };
 
-recordBtn.addEventListener("click", handleRecord);
+init();
+
+recordBtn.addEventListener("click", handleStart);
