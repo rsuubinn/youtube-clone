9d301b82c8d00de4f1ddca1f60edd775c078ccd9
rsuubinn <rsuubinn@gmail.com>
2022-05-15 19:20:31 +0900

    #16.2 Comment Box

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const BASE_JS = "./src/client/js/";
+
 module.exports = {
   mode: "development",
   plugins: [
@@ -11,9 +13,10 @@ module.exports = {
   watch: true,
   // 변경할 파일 경로
   entry: {
-    main: "./src/client/js/main.js",
-    videoPlayer: "./src/client/js/videoPlayer.js",
-    recorder: "./src/client/js/recorder.js",
+    main: BASE_JS + "main.js",
+    videoPlayer: BASE_JS + "videoPlayer.js",
+    recorder: BASE_JS + "recorder.js",
+    commentSection: BASE_JS + "commentSection.js",
   },
   output: {
     filename: "js/[name].js", // 변경 후 파일 이름
