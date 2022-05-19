4da47ca2d40f7e003024d0b5ca2afc59506ecfec
rsuubinn <rsuubinn@gmail.com>
2022-05-19 22:40:09 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/Video";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const handleListening = () => console.log(`http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
